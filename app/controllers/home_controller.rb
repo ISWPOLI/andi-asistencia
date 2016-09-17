4afd565e5e5dc8039c496a7d0c1a8857715df979
Pablo Vallejo <pabloninety@gmail.com>
2016-09-16 20:27:51 -0500

    :memmo: Add descriptive comment to action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
 
+    #
+    # This action takes care of showing the main
+    # page for the application, here, we're showing
+    # the actions that the user can take, Generating reports
+    # importing files and more.
+    #
     def index
     end
 
