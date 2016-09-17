a4fbaf631fc67958d8606c45f1e820bb68bc9a66
Pablo Vallejo <pabloninety@gmail.com>
2016-09-17 10:57:09 -0500

    :white_check_mark: Add tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @user = users(:one)
+  end
+
+  test "should redirect when logged out" do
+    get root_path
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should open index when logged in" do
+    sign_in @user
+
+    get root_path
+    assert_response :success
+  end
+
 end
