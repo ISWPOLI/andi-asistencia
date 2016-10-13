f0cba9e1ff6b0c8861d53de3be25d006d557f397
Pablo Vallejo <pabloninety@gmail.com>
2016-10-13 11:00:32 -0500

    :fire: :sunny: Create list of clients

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,6 +1,15 @@
 class ClientController < ApplicationController
 
   def index
+    @client_list = Client.where(is_active: true)
+
+    if params[:status] == 'inactive'
+      @client_list = Client.where(is_active: false)
+    end
+  end
+
+  def show
+    @client = Client.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Authentication.
-  devise_for :users
-
   # Site root.
   root 'home#index'
+
+  # Authentication.
+  devise_for :users
 
   # Clients
   resources :client
