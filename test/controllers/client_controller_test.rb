ba618b78b0df7cb3567bf19a8ecd332fc16d1b49
Pablo Vallejo <pabloninety@gmail.com>
2016-11-16 20:53:11 -0500

    :white_check_mark: Add tests

--- test/controllers/client_controller_test.rb
+++ test/controllers/client_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class ClientControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @user = users(:one)
+    sign_in @user
+
+    # Create two active clients.
+    Client.objects.create(
+      first_name: 'John',
+      last_name: 'Doe',
+      is_active: true
+    )
+
+    Client.objects.create(
+      first_name: 'Jenny',
+      last_name: 'Doe',
+      is_active: true
+    )
+  end
+
+  test "client list" do
+    get client_index_path
+    assert_container
+
+  end
+
 end
