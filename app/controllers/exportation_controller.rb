76bc73f222d6d7c5585595f1da1e140ce4b4c87e
Pablo Vallejo <pabloninety@gmail.com>
2016-11-19 18:05:30 -0500

    :sunny: Start working on exportation

--- app/controllers/exportation_controller.rb
+++ app/controllers/exportation_controller.rb
@@ -1,2 +1,17 @@
 class ExportationController < ApplicationController
+
+  def generate_bills
+  end
+
+
+  #
+  # These files are generated with clients that have no payments
+  # in an specific range.
+  #
+  def generate_charges
+    xls_path = ActionController::Base.helpers.asset_path('spreadsheets/archivo_de_cobros.xlsx')
+    xlsx = Roo::Spreadsheet.open(xls_path)
+    send_data xlsx.to_csv
+  end
+
 end
